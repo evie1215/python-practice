01fe185f278a6c9c2dc46686022d58c930c94fc3
Evelyn Yen <iluvbangtan@gmail.com>
2022-06-05 15:08:19 +0800

    Initial Commit

---
+++ practice_20220614.py
@@ -0,0 +1,24 @@
+message = "QWERT"
+
+# print(message.lower())
+
+# message_1 = "QWERT"
+
+# print(message_1.upper())
+
+# print(message.count('T'))
+
+# print(message.find('T'))
+
+# new_message = message.replace("QWERT", "QWERTY")
+# print(new_message)
+
+greeting ="HELLO"
+
+name = "Michael"
+
+message_2 = greeting + ', ' + name
+
+print(message_2)
+
+message+3 = '{}, {}. Welcome!'
